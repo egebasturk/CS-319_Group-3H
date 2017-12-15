384499297119a32b65bfa5b324f3a0a3948e8b4d
Emre Gürçay <egurcay@gmail.com>
2017-12-15 11:32:13 +0300

    Obstacle implement and attacker 2 image

--- test1/src/Assets.java
+++ test1/src/Assets.java
@@ -4,10 +4,12 @@ public class Assets
     public static String star = "src/main/resources/star.png";
     public static String tower1 = "src/main/resources/tower1.png";
     public static String tower2 = "src/main/resources/tower2.png";
+    public static String attacker1 = "src/main/resources/attacker2.png";
     public static String attacker2 = "src/main/resources/attacker2.png";
     public static String starTxt = "src/main/resources/starTxt.txt";
     public static String hero1 = "src/main/resources/hero1.png";
     public static String hero2 = "src/main/resources/hero2.png";
     public static String kapow = "src/main/resources/kapow.png";
     public static String kapow2 = "src/main/resources/kapow2.png";
+    public static String rock = "src/main/resources/rock.png";
 }
