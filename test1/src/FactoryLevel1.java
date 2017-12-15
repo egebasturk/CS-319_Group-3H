3d7268dafeb938317ca52a3f71e3e2059aac5fb4
Alp Ege Baştürk <alp.ege.basturk@gmail.com>
2017-12-15 22:19:14 +0300

    Matrix read operation was given to another class to prevent code repetition.

--- test1/src/FactoryLevel1.java
+++ test1/src/FactoryLevel1.java
@@ -14,11 +14,16 @@ public class FactoryLevel1 extends AbstractFactory {
     FactoryLevel1( )
     {
         this.level = 1;
+        typeMatrix = new int[mapHeight][mapWidth];
         // TODO: implement an algorithm to calculate
         attackers = new Attacker[15];
-        tiles = new Tile[mapHeight][mapWidth];
 
         endColumn = mapWidth - 1;
+        for ( int i = 0; i < mapHeight; i++ )
+        {
+            if ( typeMatrix[i][endColumn] == 0)
+                endRow = i;
+        }
     }
     @Override
     public Attacker[] createAttackers()
@@ -38,37 +43,7 @@ public class FactoryLevel1 extends AbstractFactory {
     @Override
     public Tile[][] createTiles()
     {
-        typeMatrix = new int[mapHeight][mapWidth];
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/map1.mat"));
-            // Reads according to octaves output format
-            // May directly read rows and columns from the file.
-            for ( int i = 0; i < 5 ; i++) {
-                String tmpLine = bufferedReader.readLine();
-                System.out.println(tmpLine);
-            }
-            for (int y = 0; y < mapHeight; y++)
-            {
-                String []row = bufferedReader.readLine().trim().split(" ");
-                for (int x = 0; x < mapWidth; x++) {
-                    typeMatrix[y][x] = Integer.parseInt(row[x]);
-                }
-            }
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-            System.out.println("Error loading map");
-            System.exit(-1);
-        }
-
-        for ( int y = 0; y < mapHeight; y++)
-        {
-            for ( int x = 0; x < mapWidth;x++)
-                tiles[y][x] = new Tile(x * tileEdge, y * tileEdge,
-                        tileEdge, tileEdge, 0, typeMatrix[y][x]);
-        }
-        System.out.println("Tiles Created");
-        return tiles;
+        return mapMatrixReader.readMapFromMatrix(mapHeight,mapWidth,tileEdge);
     }
 
     @Override
