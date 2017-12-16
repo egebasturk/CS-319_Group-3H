3e93bc7a718bf13927740548490962ede32d81a9
Alp Ege Baştürk <alp.ege.basturk@gmail.com>
2017-12-16 13:01:39 +0300

    Revert "Merge pull request #29 from egebasturk/egebasturk"
    This reverts commit a0efc8e9c45ea29a4047a4344d7d2a32f24aaf59, reversing
    changes made to 63ce6181580d89333b96ac1090586d683572ea6f.

--- test1/src/Attacker.java
+++ test1/src/Attacker.java
@@ -178,9 +178,9 @@ public class Attacker extends GameObject
                                 <= GameMap.tiles[yPosTile + 1][xPosTile].getY() &&
                         GameMap.tiles[yPosTile + 1][xPosTile].isBlocking())
                 {
-                    // check right if it is also blocking, change direction to left
-                    if (this.xPos + GameMap.tileEdge + speed
-                            >= GameMap.tiles[yPosTile][xPosTile + 1].getX() &&
+                    // check right if it is also blocking, change direction to down
+                    if (this.xPos + boxEdge + speed
+                            >= GameMap.tiles[yPosTile][xPosTile + 1].getBounds().getX() + boxEdge &&
                             GameMap.tiles[yPosTile][xPosTile + 1].isBlocking())
                     {
                         currentDirection = direction.left;
