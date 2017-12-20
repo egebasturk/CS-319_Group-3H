69539865ff0446ffb48cfe35cfa1366098e83921
Alp Ege Baştürk <alp.ege.basturk@gmail.com>
2017-12-20 17:03:24 +0300

    Reading images for jar generation fixed further. New jar created

--- test1/src/SingleAttackTower.java
+++ test1/src/SingleAttackTower.java
@@ -32,7 +32,7 @@ public class SingleAttackTower extends Tower {
 
         try {
             // TODO: Implement better resource loading methods
-            image = ImageIO.read(new File(Assets.tower2));
+            image = ImageIO.read(getClass().getResource(Assets.tower2));
         }
         catch (IOException e) {
             e.printStackTrace();
